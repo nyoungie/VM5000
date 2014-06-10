74790f2b9bf83bbec5a5455694a7aac5e5db6683
nyoungie <kayan.lam@web.de>
2014-06-11 01:07:45 +0200

    -

--- src/RegistrierungFenster.java
+++ src/RegistrierungFenster.java
@@ -9,42 +9,39 @@ public class RegistrierungFenster {
         JFrame registrierungsFenster = new JFrame("Anmeldung");
         registrierungsFenster.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //Panel wo alle Elemente drin sind in einem GridBagLayout
-        JPanel registrierungspanel = new JPanel();
-        GridBagLayout gbl = new GridBagLayout();
-        registrierungspanel.setLayout(gbl);
+        //Hintergrundbild
+        Panel registrierungsBg = new Panel("C:\\Users\\user\\IdeaProjects\\VM5000\\src\\img\\registrierungBg.png");
 
-        //JLabel erstellt und Schriftart verändert
-        JLabel neuanmeldung = new JLabel("Neuanmeldung", JLabel.CENTER);
-        neuanmeldung.setFont(new Font("Agency FB", Font.BOLD ,30));
-
-        JLabel id_label = new JLabel("ID: ");
-        JLabel pw_label = new JLabel("PASSWORT: ");
-        JLabel pwWH_label = new JLabel("PASSWORT Wh.: ");
+        //Panel
+        JPanel registrierungsPanel = new JPanel(new GridBagLayout());
+        registrierungsPanel.setOpaque(false);
 
         //Textfelder zum Abfragen der Daten
-        JTextField id_txt = new JTextField();
-        JTextField pw_txt = new JTextField();
-        JTextField pwWH_txt = new JTextField();
+        JTextField idText = new JTextField(12);
+        JTextField pwText = new JTextField(12);
+        JTextField pwNochmalText = new JTextField(12);
 
-        //Anmeldebutton wird hier erstellt
-        JButton signUp_btn = new JButton("SIGN UP");
+        //Buttons werden hier erstellt
+        ImageIcon zumMenuIcon = new BildBauer().createImageIcon("img/cancelButton.png");
+        Button zumMenu = new Button(zumMenuIcon,400);
 
-        //hier werden alle Elemente dem registrierungspanel hinzugefügt
-        registrierungspanel.add(neuanmeldung,new GridBagConstraints(0,0,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0));
-        registrierungspanel.add(id_label,new GridBagConstraints(0,1,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0));
-        registrierungspanel.add(id_txt,new GridBagConstraints(1,1, 1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),150,0));
-        registrierungspanel.add(pw_label,new GridBagConstraints(0,2,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0));
-        registrierungspanel.add(pw_txt,new GridBagConstraints(1,2,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),150,0));
-        registrierungspanel.add(pwWH_label,new GridBagConstraints(0,3,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0));
-        registrierungspanel.add(pwWH_txt,new GridBagConstraints(1,3,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),150,0));
-        registrierungspanel.add(signUp_btn,new GridBagConstraints(0,4,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0));
+        ImageIcon signUpIcon = new BildBauer().createImageIcon("img/signUpButton.png");
+        Button signUp = new Button(signUpIcon,400);
+
+        //hier werden alle Elemente dem registrierungsPanel hinzugefügt
+        registrierungsPanel.add(zumMenu,new GridBagConstraints(0,1,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,350,0,0),0,0));
+        registrierungsPanel.add(idText,new GridBagConstraints(0,2,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(120,150,0,0),0,0));
+        registrierungsPanel.add(pwText,new GridBagConstraints(0,3,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(13,150,0,0),0,0));
+        registrierungsPanel.add(pwNochmalText,new GridBagConstraints(0,4,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(12,150,0,0),0,0));
+        registrierungsPanel.add(signUp,new GridBagConstraints(0,5,0,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(40,0,0,0),0,0));
+
+        registrierungsBg.add(registrierungsPanel);
 
         //Komponenten zum Fenster hinzufügen
-        registrierungsFenster.add(registrierungspanel);
+        registrierungsFenster.add(registrierungsBg);
 
         //Fenstergröße setzen und anzeigen lassen
-        registrierungsFenster.setSize(400, 300);
+        registrierungsFenster.setSize(415, 400);
         registrierungsFenster.setVisible(true);
 
     }
